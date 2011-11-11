44517ad71232ef83aac9c16eadf1a1b53737ee8a
Karl Hanzel <karl@khaos.com>
2011-11-10 19:41:59 -0800

    Initial Check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitchenPost::Application.config.secret_token = '54e52c3f5122754331beca9df557903c5769c3427c8fb1273d0a3f3b759ab003403bf94aa01420ffe12900bbfb431022ae76ecc1a7cd8ce147918d618158301a'
